a85c2b9f9952b1c8d15ae5ce9ec79380e9d44e39
Tyler <taehyun9202@hotmail.com>
2020-03-13 10:29:43 -0400

    second commit

--- src/components/homeComponent.js
+++ src/components/homeComponent.js
@@ -5,27 +5,26 @@ import axios from 'axios';
 
 
 const HomeComponent = (props) => {
-  const [search, setSearch] = useState({category:"", id:""})
+  const [search, setSearch] = useState({category:"--Select--", id:""})
   const [display, setDisplay] = useState({})
+  
+
   const submitSearch = e =>{
     e.preventDefault();
     console.log(search);
-    setSearch({
-      ...search,
-      [e.target.name]: e.target.value
-  });
+
     let nav = '/' + search.category + '/'+ search.id
     navigate(nav);
 
     let navapi = "https://swapi.co/api/" + search.category + '/'+ search.id
+    setSearch({category:"--Select--", id:""})
     console.log(navapi);
     axios.get(navapi)
       
         .then(res => {
           console.log(res.data);
           setDisplay(res.data)})
-        .catch(err => {console.log(err)},[])
-        
+        .catch(err => {console.log(err)},[])        
   }
   console.log(display)
 
@@ -42,17 +41,16 @@ const HomeComponent = (props) => {
       <div>
         <form onSubmit={submitSearch}>
         <h1>Search for:</h1>
-        <select name="category" onChange={onChange}>
+        <select name="category" onChange={onChange} value={search.category}>
           <option disabled selected>--Select--</option>
           <option value="people">People</option>
           <option value="planets">Planet</option>
           <option value="spaceships">Spaceship</option>
         </select>
         <h1>ID:</h1>
-        <input type="text" name="id" onChange={onChange}></input>
+        <input type="text" name="id" onChange={onChange} value={search.id}></input>
         <input type="submit" value="Search" />  
         </form>        
-
 
         <ul>
           {keys.map((key) => 
@@ -60,12 +58,9 @@ const HomeComponent = (props) => {
           )}
         </ul>
 
-
-         {/* <ul>
-          {display.map((i) => 
-            <li> {i}</li>
-          )}
-        </ul> */}
+        {/* {Object.keys(display).map((key) => ( 
+          <p>{key}: {display[key]}</p>
+        ))} */}
       </div>
 
   );
